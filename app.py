a6adc62d5c476910deab1347e96302b1b6823c7d
Vesa Berdyna <vesaberdyna@gmail.com>
2022-12-27 21:47:41 +0100

    Create board for the player

--- app.py
+++ app.py
@@ -53,3 +53,41 @@ def winning_move(board, piece):
 		for r in range(3, ROW_COUNT):
 			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
 				return True
+			
+	def draw_board(board):
+	for c in range(COLUMN_COUNT):
+		for r in range(ROW_COUNT):
+			pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+			pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+	
+	for c in range(COLUMN_COUNT):
+		for r in range(ROW_COUNT):		
+			if board[r][c] == 1:
+				pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+			elif board[r][c] == 2: 
+				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+	pygame.display.update()
+
+
+	board = create_board()
+	print_board(board)
+	game_over = False
+	turn = 0
+
+	pygame.init()
+
+	SQUARESIZE = 100
+
+	width = COLUMN_COUNT * SQUARESIZE
+	height = (ROW_COUNT+1) * SQUARESIZE
+
+	size = (width, height)
+
+	RADIUS = int(SQUARESIZE/2 - 5)
+
+	screen = pygame.display.set_mode(size)
+	draw_board(board)
+	pygame.display.update()
+
+	myfont = pygame.font.SysFont("monospace", 75)		
+			
