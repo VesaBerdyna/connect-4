8c7121469453cb2318b29ce9a8e7ebf514392666
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2023-01-21 10:39:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,148 +1,100 @@
-import numpy as np
-import pygame
-import sys
-import math
+from flask import Flask, jsonify, redirect, render_template, request, session, url_for
 
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
+from db.game_db import GameDB
+from models.game import Game
+from models.player_factory import PlayerFactory
+from exceptions.game_existment import GameExistment
+from exceptions.empty_game import EmptyGame
 
-ROW_COUNT = 6
-COLUMN_COUNT = 7
+app = Flask(__name__)
+app.secret_key = "secret_key"
 
-def create_board():
-	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
-	return board
-
-def drop_piece(board, row, col, piece):
-	board[row][col] = piece
-
-def is_valid_location(board, col):
-	return board[ROW_COUNT-1][col] == 0
-
-def get_next_open_row(board, col):
-	for r in range(ROW_COUNT):
-		if board[r][col] == 0:
-			return r
-
-def print_board(board):
-	print(np.flip(board, 0))
-
-def winning_move(board, piece):
-	# Check horizontal locations for win
-	for c in range(COLUMN_COUNT-3):
-		for r in range(ROW_COUNT):
-			if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
-				return True
-
-	# Check vertical locations for win
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT-3):
-			if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
-				return True
-
-	# Check positively sloped diaganols
-	for c in range(COLUMN_COUNT-3):
-		for r in range(ROW_COUNT-3):
-			if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
-				return True
-
-	# Check negatively sloped diaganols
-	for c in range(COLUMN_COUNT-3):
-		for r in range(3, ROW_COUNT):
-			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
-				return True
-			
-def draw_board(board):
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):
-			pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
-			pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
-	
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):		
-			if board[r][c] == 1:
-				pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-			elif board[r][c] == 2: 
-				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-	pygame.display.update()
+game_db = GameDB("GAME")
+game_db.init_db()
 
 
-board = create_board()
-print_board(board)
-game_over = False
-turn = 0
-
-pygame.init()
-
-SQUARESIZE = 100
-
-width = COLUMN_COUNT * SQUARESIZE
-height = (ROW_COUNT+1) * SQUARESIZE
-
-size = (width, height)
-
-RADIUS = int(SQUARESIZE/2 - 5)
-
-screen = pygame.display.set_mode(size)
-draw_board(board)
-pygame.display.update()
-
-myfont = pygame.font.SysFont("monospace", 75)		
-
-while not game_over:
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-
-		if event.type == pygame.MOUSEMOTION:
-			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-			posx = event.pos[0]
-			if turn == 0:
-				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
-			else: 
-				pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
-		pygame.display.update()
-
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-			#print(event.pos)
-			# Ask for Player 1 Input
-			if turn == 0:
-				posx = event.pos[0]
-				col = int(math.floor(posx/SQUARESIZE))
-
-				if is_valid_location(board, col):
-					row = get_next_open_row(board, col)
-					drop_piece(board, row, col, 1)
-
-					if winning_move(board, 1):
-						label = myfont.render("Player 1 wins!!", 1, RED)
-						screen.blit(label, (40,10))
-						game_over = True
+@app.route("/")
+def start_game():
+    return render_template("start_game.html")
 
 
-			# # Ask for Player 2 Input
-			else:				
-				posx = event.pos[0]
-				col = int(math.floor(posx/SQUARESIZE))
+@app.route("/modes")
+def choose_mode():
+    return render_template("choose_mode.html")
 
-				if is_valid_location(board, col):
-					row = get_next_open_row(board, col)
-					drop_piece(board, row, col, 2)
 
-					if winning_move(board, 2):
-						label = myfont.render("Player 2 wins!!", 1, YELLOW)
-						screen.blit(label, (40,10))
-						game_over = True
+@app.route("/popup", methods=["POST", "GET"])
+def popup():
+    if request.method == "GET":
+        session["mode"] = request.args.get("mode")
+        mode = session["mode"]
+        return render_template("popup.html", mode=mode)
+    if request.method == "POST":
+        mode = request.form["mode"]
+        player_one_name = request.form["player_one"]
+        player_two_name = request.form["player_two"]
+        print(player_one_name, player_two_name)
 
-			print_board(board)
-			draw_board(board)
+        player_one, player_two = PlayerFactory.create_player(
+            mode, player_one_name=player_one_name, player_two_name=player_two_name
+        )
 
-			turn += 1
-			turn = turn % 2
+        session["player_one"] = player_one.to_dict()
+        session["player_two"] = player_two.to_dict()
 
-			if game_over:
-				pygame.time.wait(3000)
+        return redirect(url_for("create_game"))
+    return render_template("popup.html")
+
+
+@app.route("/game")
+def create_game():
+    global game
+    player_one = PlayerFactory.create_player_from_json(session["player_one"])
+    player_two = PlayerFactory.create_player_from_json(session["player_two"])
+
+    game = Game(player_one=player_one, player_two=player_two)
+    game.set_player_dot()
+    game.set_players()
+    game.start()
+    game_id = game.get_id()
+    game_db.create_game(game_id, game.get_game_state())
+    return render_template("game.html", game_id=game_id)
+
+
+@app.route("/board", methods=["GET"])
+def board():
+    game_id = request.args.get("game_id")
+    if game_id is None:
+        EmptyGame
+
+    game_state = game_db.get_game_state(game_id)
+    if game_state is None:
+        GameExistment
+
+    print("gameeeeeeeeeeeeeeeeeeeeeeeeeeee:", game_state)
+
+    response = jsonify(
+        {
+            "game_state": game_state[1],
+        }
+    )
+    return response
+
+
+@app.route("/move", methods=["POST"])
+def move():
+    print("in move routeeeeeeeeeeee")
+    if request.method == "POST":
+        col = int(request.get_json()["column"])
+        # col = int(request.get_json()["column"]) - 1
+        print("coooooooooool", col)
+        print("typeeeeeee", type(col))
+        print(game.get_game_state())
+        res = game.play(player=game.current_turn, col=col)
+        game_state = game.get_game_state()
+        print(game_state)
+        game_db.update_game(game.get_id(), game_state)
+    return game_state
+
+if __name__ == "__main__":
+    app.run(debug=True)
