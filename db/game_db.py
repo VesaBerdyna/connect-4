1b8f2fcd6db509f976db6f3989544a07aee63864
AritaAlidemaj <58113020+AritaAlidemaj@users.noreply.github.com>
2023-01-28 23:23:55 +0100

    added state design pattern for game db

--- db/game_db.py
+++ db/game_db.py
@@ -33,9 +33,22 @@ class Database(ABC):
         pass
 
 
+class GameState:
+    def __init__(self):
+        self.state = None
+
+    def update_game(self, data):
+        self.state = data
+
+    def get_game_state(self):
+        return self.state
+
+
 class GameDB(Database):
     def __init__(self, table_name):
         self.table_name = table_name
+        self.game_state = GameState()
+
 
     def init_db(self):
         connection = self._get_connection()
@@ -43,7 +56,7 @@ class GameDB(Database):
             connection.execute(
                 f"CREATE TABLE IF NOT EXISTS {self.table_name} (ID TEXT PRIMARY KEY, DATA TEXT)"
             )
-            print(f"Table {self.table_name} created.")
+            self.game_state.update_game(f"Table {self.table_name} created.")
         except sqlite3.Error as e:
             raise Exception(f"Error creating table: {e}")
 
@@ -53,10 +66,9 @@ class GameDB(Database):
             cur = connection.cursor()
             cur.execute(f"INSERT INTO {self.table_name} VALUES (?, ?)", (id, data))
             connection.commit()
-            print("First move added to the table.")
+            self.game_state.update_game(f"First move added to the {self.table_name} table.")
         except sqlite3.Error as e:
-            print(e)
-            # raise Exception(f"Error adding move: {e}")
+            raise Exception(f"Error adding move: {e}")
 
     def update_game(self, id, data):
         connection = self._get_connection()
@@ -64,11 +76,9 @@ class GameDB(Database):
             cur = connection.cursor()
             cur.execute(f"UPDATE {self.table_name} SET DATA=? WHERE ID=?", (data, id))
             connection.commit()
-            print("Player's move updated to db.")
-
+            self.game_state.update_game("Player's move updated to db.")
         except sqlite3.Error as e:
-            print(e)
-            # raise Exception(f"Error adding move: {e}")
+            raise Exception(f"Error adding move: {e}")
 
     def get_game_state(self, id):
         connection = self._get_connection()
@@ -77,22 +87,23 @@ class GameDB(Database):
             cur.execute(f"SELECT * FROM {self.table_name} WHERE ID='{id}'")
             result = cur.fetchall()
             # print("result from db", result, id)
+            self.game_state.update_game("Retrieved game state.")
             return result[-1]
         except sqlite3.Error as e:
-            # raise Exception(f"Error retrieving move: {e}")
-            print(e)
+            raise Exception(f"Error retrieving move: {e}")
 
     def clear(self):
         connection = self._get_connection()
         try:
             connection.execute(f"DROP TABLE IF EXISTS {self.table_name}")
-            print("Table dropped.")
+            self.game_state.update_game(f"Table {self.table} dropped.")
         except sqlite3.Error as e:
             raise Exception(f"Error dropping table: {e}")
 
     def _get_connection(self):
         try:
             connection = sqlite3.connect("game_db")
+            self.game_state.update_game(f"Created connection with sqlite db.")
             return connection
         except sqlite3.Error as e:
             raise Exception(f"Error connecting to database: {e}")
