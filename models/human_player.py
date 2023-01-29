6b9d538751908f4b6a0360490ed570e88128cbed
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2023-01-29 15:53:59 +0100

    Update human_player.py

--- models/human_player.py
+++ models/human_player.py
@@ -1,19 +1,17 @@
-from models.player import Player
+from models.player import Player	
+from enums.player_type import PlayerType	
 
-
-class HumanPlayer(Player):
-    def __init__(self, name, id=None):
-        super().__init__(name, id)
-
-    def make_move(self, board, row, col, dot):
-        """Ask the player for a move and make it on the game board"""
-        board.drop_dot(row, col, dot)
-
-    def to_dict(self):
-        return {"id": self.id, "name": self.name}
-
-    @classmethod
-    def from_dict(cls, data):
-        id = data.get("id")
-        name = data.get("name")
+class HumanPlayer(Player):	
+    def __init__(self, name: str, id=None):	
+        super().__init__(name, id)	
+        self.type = PlayerType.HumanPlayer.value	
+    def make_move(self, board, row, col, dot):	
+        """Ask the player for a move and make it on the game board"""	
+        board.drop_dot(row, col, dot)	
+    def to_dict(self):	
+        return {"id": self.id, "name": self.name, "type": self.type}	
+    @classmethod	
+    def from_dict(cls, data):	
+        id = data.get("id")	
+        name = data.get("name")	
         return cls(name=name, id=id)
