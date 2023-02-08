6b9a78859236db50f2b7399186fdc1a5078126eb
Vesa Berdyna <vesaberdyna@gmail.com>
2023-02-08 21:58:13 +0100

    Replace the minmax bot with the random bot player

--- models/player_factory.py
+++ models/player_factory.py
@@ -1,5 +1,5 @@
 from models.human_player import HumanPlayer
-from models.minimax_ai import MinimaxAI
+from models.bot import MinimaxAI, RandomAI
 from enums.player_type import PlayerType
 
 
@@ -9,13 +9,11 @@ class PlayerFactory:
         if mode == "multi":
             return HumanPlayer(player_one_name), HumanPlayer(player_two_name)
         else:
-            return HumanPlayer(player_one_name), MinimaxAI("Bot")
+            return HumanPlayer(player_one_name), RandomAI("Bot")
 
     @staticmethod
     def create_player_from_json(data: str):
         if data["type"] == PlayerType.HumanPlayer.value:
             return HumanPlayer.from_dict(data)
         else:
-            return MinimaxAI.from_dict(data)
-
-    
+            return RandomAI.from_dict(data)
